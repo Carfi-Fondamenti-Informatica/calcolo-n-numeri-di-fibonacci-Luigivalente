cae69684c1970351d1cf82c0598ad437ff9212f7
Luigivalente <91776312+Luigivalente@users.noreply.github.com>
2021-11-12 23:26:39 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,20 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int n=0,n1=0,n2=1;
+    int risultato;
+    cin >> n;
+    if (n<2){
+        cout << "errore"<<endl;
+    }else{
+        cout << "1" <<endl;
+        for(int i=0; i<n-1; i++){
+            risultato=n1+n2;
+            n1=n2;
+            n2=risultato;
+            cout << risultato <<endl;
+        }
+    }
+
+    return 0;
 }
